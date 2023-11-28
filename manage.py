c3ff83dc87faa6ee2f62884b42b2fbd65c13a582
YONES <107154559+ynstf@users.noreply.github.com>
2023-11-28 01:25:29 +0100

    deploy

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-#import dotenv
+import dotenv
 
 
 def main():
@@ -20,5 +20,5 @@ def main():
 
 
 if __name__ == '__main__':
-    #dotenv.read_dotenv()
+    dotenv.read_dotenv()
     main()
